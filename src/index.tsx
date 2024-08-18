d4e428b543553bf633e0b3f6c0c3cbce35d82bc2
Michael Klen <nitrex.code@gmail.com>
2024-08-18 13:42:27 +0200

    edit index file moved provider from app here

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 
-import './index.css'
+import store from './redux/store'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const rootElement = document.getElementById('root')!
+const root = ReactDOM.createRoot(rootElement)
+
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
 )
-
-reportWebVitals()
