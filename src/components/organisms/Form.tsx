801d8d8d657c499cc35520bba41f84d77488c51e
Michael Klen <nitrex.code@gmail.com>
2024-08-18 13:43:41 +0200

    create Form component as organism

---
+++ src/components/organisms/Form.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+import { RootState } from '../../redux/store'
+import updateForm from '../../redux/formSlice'
+import { FormFields } from '../'
+import { Button } from '../'
+
+const Form: React.FC = () => {
+  const formData = useSelector((state: RootState) => state.form)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(updateForm({ [e.target.name]: e.target.value }))
+  }
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    localStorage.setItem('formData', JSON.stringify(formData))
+
+    const fileData = new Blob([JSON.stringify(formData)], {
+      type: 'application/json',
+    })
+    const url = URL.createObjectURL(fileData)
+    const link = document.createElement('a')
+    link.href = url
+    link.download = 'formData.json'
+    link.click()
+
+    navigate('/thanks') // Redirect to Thank You page
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <FormFields formData={formData} handleChange={handleChange} />
+      <Button text="Submit" type="submit" />
+    </form>
+  )
+}
+
+export default Form
